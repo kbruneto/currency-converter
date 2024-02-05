2f796c708f36d08ac9cfdc9e765e3f4c4da36660
kbruneto <hizztxt@gmail.com>
2024-02-05 19:54:49 -0300

    alteracao change

--- script.js
+++ script.js
@@ -234,25 +234,26 @@ function atualizarIMG1() {
         imagem1.style.src = "./assets/euro.png"
         textoMoedasAConverter.innerHTML = ("€ 0.0")
     }
+
     if (primeiraMoeda.value == "dolar") {
         textoPrimeiraMoeda.innerHTML = ("Dolar")
         imagem1.style.src = "./assets/dolar.png"
         textoMoedasAConverter.innerHTML = ("US$ 0.0")
-
-
     }
+
     if (primeiraMoeda.value == "libra") {
         textoPrimeiraMoeda.innerHTML = ("£ Libra")
         imagem1.style.src = "./assets/libra.png"
         textoMoedasAConverter.innerHTML = ("£ 0.0")
-
-
-    } else if (primeiraMoeda.value == "bitcoin") {
+    }
+    
+    if (primeiraMoeda.value == "bitcoin") {
         textoPrimeiraMoeda.innerHTML = ("₿ Bitcoin")
         imagem1.style.src = "./assets/bitcoin.png"
         textoMoedasAConverter.innerHTML = ("BTC 0.0")
-
-    } else if (primeiraMoeda.value == "real") {
+    }
+    
+    if (primeiraMoeda.value == "real") {
         textoPrimeiraMoeda.innerHTML = ("R$ Real Brasileiro")
         imagem1.style.src = "./assets/real.png"
         textoMoedasAConverter.innerHTML = ("R$ 00,00")
@@ -266,30 +267,31 @@ function atualizarIMG2() {
         imagem2.style.src = "./assets/euro.png"
         textoMoedasConvertidas.innerHTML = ("€ 0.0")
     }
+
     if (segundaMoeda.value == "dolar") {
         textoSegundaMoeda.innerHTML = ("U$ Dolar Americano")
         imagem2.style.src = "./assets/dolar.png"
         textoMoedasConvertidas.innerHTML = ("US$ 0.0")
-
-
     }
+
     if (segundaMoeda.value == "libra") {
         textoSegundaMoeda.innerHTML = ("£ Libra")
         imagem2.style.src = "./assets/libra.png"
         textoMoedasConvertidas.innerHTML = ("£ 0.0")
-
-
-    } else if (segundaMoeda.value == "bitcoin") {
+    }
+    
+    if (segundaMoeda.value == "bitcoin") {
         textoSegundaMoeda.innerHTML = ("₿ Bitcoin")
         imagem2.style.src = "./assets/bitcoin.png"
         textoMoedasConvertidas.innerHTML = ("BTC 0.0")
-    } else if (segundaMoeda.value == "real") {
+    }
+    
+    if (segundaMoeda.value == "real") {
         textoSegundaMoeda.innerHTML = ("R$ Real Brasileiro")
         imagem2.style.src = "./assets/real.png"
         textoMoedasConvertidas.innerHTML = ("R$ 00,00")
     }
-
-
+    
 }
 
 primeiraMoeda.addEventListener("change", atualizarIMG1); //toda vez que eu trocar de moeda atualiza as imagens
