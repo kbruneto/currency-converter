adc03a476f107831751907805275949658a93d55
kbruneto <hizztxt@gmail.com>
2024-02-05 19:51:10 -0300

    alteração nomes moedas if change

--- script.js
+++ script.js
@@ -230,7 +230,7 @@ botaoDeConversao.addEventListener("click", convertValue);
 function atualizarIMG1() {
 
     if (primeiraMoeda.value == "euro") {
-        textoPrimeiraMoeda.innerHTML = ("Euro")
+        textoPrimeiraMoeda.innerHTML = ("€ Euro")
         imagem1.style.src = "./assets/euro.png"
         textoMoedasAConverter.innerHTML = ("€ 0.0")
     }
@@ -242,49 +242,49 @@ function atualizarIMG1() {
 
     }
     if (primeiraMoeda.value == "libra") {
-        textoPrimeiraMoeda.innerHTML = ("Libra")
+        textoPrimeiraMoeda.innerHTML = ("£ Libra")
         imagem1.style.src = "./assets/libra.png"
         textoMoedasAConverter.innerHTML = ("£ 0.0")
 
 
     } else if (primeiraMoeda.value == "bitcoin") {
-        textoPrimeiraMoeda.innerHTML = ("Bitcoin")
+        textoPrimeiraMoeda.innerHTML = ("₿ Bitcoin")
         imagem1.style.src = "./assets/bitcoin.png"
         textoMoedasAConverter.innerHTML = ("BTC 0.0")
 
     } else if (primeiraMoeda.value == "real") {
-        textoPrimeiraMoeda.innerHTML = ("Real")
+        textoPrimeiraMoeda.innerHTML = ("R$ Real Brasileiro")
         imagem1.style.src = "./assets/real.png"
-        textoMoedasAConverter.innerHTML = ("BTC 0.0")
+        textoMoedasAConverter.innerHTML = ("R$ 00,00")
     }
 }
 
 
 function atualizarIMG2() {
     if (segundaMoeda.value == "euro") {
-        textoSegundaMoeda.innerHTML = ("Euro")
+        textoSegundaMoeda.innerHTML = ("€ Euro")
         imagem2.style.src = "./assets/euro.png"
         textoMoedasConvertidas.innerHTML = ("€ 0.0")
     }
     if (segundaMoeda.value == "dolar") {
-        textoSegundaMoeda.innerHTML = ("dolar")
+        textoSegundaMoeda.innerHTML = ("U$ Dolar Americano")
         imagem2.style.src = "./assets/dolar.png"
         textoMoedasConvertidas.innerHTML = ("US$ 0.0")
 
 
     }
     if (segundaMoeda.value == "libra") {
-        textoSegundaMoeda.innerHTML = ("Libra")
+        textoSegundaMoeda.innerHTML = ("£ Libra")
         imagem2.style.src = "./assets/libra.png"
         textoMoedasConvertidas.innerHTML = ("£ 0.0")
 
 
     } else if (segundaMoeda.value == "bitcoin") {
-        textoSegundaMoeda.innerHTML = ("Bitcoin")
+        textoSegundaMoeda.innerHTML = ("₿ Bitcoin")
         imagem2.style.src = "./assets/bitcoin.png"
         textoMoedasConvertidas.innerHTML = ("BTC 0.0")
     } else if (segundaMoeda.value == "real") {
-        textoSegundaMoeda.innerHTML = ("Real")
+        textoSegundaMoeda.innerHTML = ("R$ Real Brasileiro")
         imagem2.style.src = "./assets/real.png"
         textoMoedasConvertidas.innerHTML = ("R$ 00,00")
     }
